78bf8920250fb7ab27c4aceeadab9cd2084a065c
mohamedfathi540 <mohamed.fathe540@gmail.com>
2026-01-17 23:41:55 +0200

    Add PGVector provider implementation and associated enums for PostgreSQL vector database integration.

--- Stores/VectorDB/VectorDBEnums.py
+++ Stores/VectorDB/VectorDBEnums.py
@@ -3,11 +3,29 @@ from enum import Enum
 class VectorDBEnums (Enum) :
 
     QDRANT = "QDRANT"
-
+    PGVECTOR = "PGVECTOR"
 
 
 class DistanceMethodEnums (Enum) :
 
     COSINE = "cosine"
     DOT = "dot"
+    
+
+class PgVectorTableSchemeEnums (Enum) :
+    ID = "id"
+    TEXT = "text"
+    VECTORS = "vectors"
+    CHUNCK_ID = "chunck_id"
+    METADATA = "metadata"
+    _PREFIX = "pgvector"
+
+
+class PgvectorDistanceMethodEnums (Enum) :
+    COSINE = "vector_cosine_eps"
+    DOT = "vector_12_eps"
+
+class PgvectorIndexTypeEnums (Enum) :
+    IVFFLAT = "ivfflat"
+    HNSW = "hnsw"
     
